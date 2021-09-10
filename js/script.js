0e66d29d1cfda35ad51a96f6575cd45b3db7c07c
osaretin <osaretin.frank10@gmail.com>
2021-09-10 07:16:06 +0100

    mobile-nav

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const m_nav = document.querySelector(".mobile-nav");
+const m_nav_menu = document.querySelector(".mobile-nav-menu");
+
+m_nav.addEventListener("click", (e)=>{
+    if (m_nav_menu.classList.contains("hide")){
+        m_nav_menu.className = "mobile-nav-menu show";
+    }
+    else{
+        m_nav_menu.className = "mobile-nav-menu hide";
+    }
+
+    
+})
